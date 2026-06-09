eff6e7599f5754cf24cd62213a8d28e10cf7ed31
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-09 17:06:30 +0530

    Full Website Completed

---
+++ src/components/Freshimg.jsx
@@ -0,0 +1,240 @@
+import React from 'react'
+import '../styles/Freshimg.css'
+
+
+const Freshimg = () => {
+    return (
+        <>
+            <div>
+                <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/Hero_Slider/SVD/6feb/1_6.jpg" id='topDealsBanner' alt="" />
+                <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/Sep21/CatStore/FV_1500X300.jpg" id='topDealsBanner' alt="" />
+            <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/May25/1500x150_stripe.gif" id='topDealsBanner' alt="" />
+                
+            </div>
+
+            <div className="grocery-container">
+
+                <h2 className="grocery-heading">
+                    Groceries & Food
+                </h2>
+                <div className="line"></div>
+
+                <div className="grocery-grid">
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Fresh_Vegetables.jpg" alt="" />
+                        {/* <p>Fresh vegetables</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Fresh_fruits.jpg" alt="" />
+                        {/* <p>Fresh fruits</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Atta_Rice__grains.jpg" alt="" />
+                        {/* <p>Atta, rice & grains</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Dals__Pulses.jpg" alt="" />
+                        {/* <p>Dals & pulses</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Oil__Ghee.jpg" alt="" />
+                        {/* <p>Oil & ghee</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img24/Fresh/Aug/V1/Masala_sugar__spices.jpg" alt="" />
+                        {/* <p>Masala, sugar & spices</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Milk__dairy.jpg" alt="" />
+                        {/* <p>Milk & dairy</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Breads__bakery.jpg" alt="" />
+                        {/* <p>Breads & bakery</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img24/Fresh/Aug/V1/Cereals__dry_fruits.jpg" alt="" />
+                        {/* <p>Cereals & dry fruits</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/Apr25/Tea_coffee__drink_mixes_BAU.jpg" alt="" />
+                        {/* <p>Tea, coffee & drink mixes</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Juices__cold_drinks.jpg" alt="" />
+                        {/* <p>Juices & cold drinks</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Sauces__spreads.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Indian-sweets_1.0_2.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Eggs_meat__fish.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Chips.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Choclate__Ice_cream.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Ready_to_eat__frozen_food.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/Indian_Snacks_1.jpg" alt="" />
+                        {/* <p>Sauces & spreads</p> */}
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+
+            <div className="grocery-container">
+
+                <h2 className="grocery-heading">
+                    Beauty & Personal Care
+                </h2>
+                <div className="line"></div>
+
+                <div className="grocery-grid">
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Bath__body.jpg" alt="" />
+                        {/* <p>Fresh vegetables</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Hair_care.jpg" alt="" />
+                        {/* <p>Fresh fruits</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Skincare.jpg" alt="" />
+                        {/* <p>Atta, rice & grains</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Deos__perfumes.jpg" alt="" />
+                        {/* <p>Dals & pulses</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Mens_grooming.jpg" alt="" />
+                        {/* <p>Oil & ghee</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Oral_care.jpg" alt="" />
+                        {/* <p>Masala, sugar & spices</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Baby_care.jpg" alt="" />
+                        {/* <p>Milk & dairy</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Makeup__cosmetics.jpg" alt="" />
+                        {/* <p>Breads & bakery</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Pharma__wellness.jpg" alt="" />
+                        {/* <p>Cereals & dry fruits</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Diet__nutrition.jpg" alt="" />
+                        {/* <p>Tea, coffee & drink mixes</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Pet_care.jpg" alt="" />
+                        {/* <p>Juices & cold drinks</p> */}
+                    </div>
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/CategoryStores/Pet/dry-fruits_1.jpg" alt="" />
+                        {/* <p>Juices & cold drinks</p> */}
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className="grocery-container">
+
+                <h2 className="grocery-heading">
+                    Household & Essentials
+                </h2>
+                <div className="line"></div>
+
+
+                <div className="grocery-grid">
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Laundry_detergents.jpg" alt="" />
+                        {/* <p>Fresh vegetables</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Household__toilet_cleaners.jpg" alt="" />
+                        {/* <p>Fresh fruits</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Cleaning_supplies.jpg" alt="" />
+                        {/* <p>Atta, rice & grains</p> */}
+                    </div>
+
+                    <div className="grocery-card">
+                        <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/SBCTILES/UPDATED/Home__kitchen.jpg" alt="" />
+                        {/* <p>Dals & pulses</p> */}
+                    </div>
+
+                   
+
+
+                </div>
+
+
+            </div>
+            <img src="https://m.media-amazon.com/images/G/31/img18/Fresh/Oct20/UNREC/1500x150_strip.jpg" id='topDealsBanner' alt="" />
+        </>
+    )
+}
+
+export default Freshimg
--- src/pages/Fresh.jsx
+++ src/pages/Fresh.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Freshimg from '../components/Freshimg'
+// import Heading from '../components/Heading'
+import Heading from '../components/Heading'
 
 const Fresh = () => {
     return (
         <div>
-            Fresh
+            <Heading heading="Cartify Fresh" desc="Enjoy the freshest products at your fingertips." />
+
+            <Freshimg />
         </div>
     )
 }
